1b29b90c1c98e848fd48e8472f5b2f4b5602ef78
Salim Boinaheri <22500188@me.uvsq.fr>
2026-02-26 11:49:06 +0100

    AJOUT DE LA MULTIPLICATION

--- calculatrice.py
+++ calculatrice.py
@@ -1,19 +1,38 @@
-def addition():
+def multiplication():
     while True:
         try:
-            
             num1 = input("Entrez le premier nombre: ")
             num2 = input("Entrez le deuxième nombre: ")
+
             num1 = float(num1)
             num2 = float(num2)
-            
-            
-            resultat = num1 + num2
-            print("resultat")
-            break  
-            
+
+            resultat = num1 * num2
+            print("Résultat :", resultat)
+            break
+
         except ValueError:
             print("Erreur : veuillez entrer des nombres valides.")
 
 
+
+def addition():
+    while True:
+        try:
+            num1 = input("Entrez le premier nombre: ")
+            num2 = input("Entrez le deuxième nombre: ")
+            num1 = float(num1)
+            num2 = float(num2)
+
+            resultatA = num1 + num2
+            print("Résultat :", resultatA)
+            break
+
+        except ValueError:
+            print("Erreur : veuillez entrer des nombres valides.")
+
+
+
+
 addition()
+multiplication()
